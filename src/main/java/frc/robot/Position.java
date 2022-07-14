8d74f62d8fd807b16e58a80b67b295a50abec93c
ArcturusXII <107654042+ArcturusXII@users.noreply.github.com>
2022-07-14 21:43:41 +0000

    fixed gremlin attack, added direction code

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -7,19 +7,22 @@ public class Position {
 
 
     public void driveUp() throws InterruptedException {
-
+driveDistance(10);
+      turnDegrees(-360);
     }
 
     public void driveDown() throws InterruptedException {
-
+driveDistance(10);
+      turnDegrees(-90);
     }
 
     public void driveRight() throws InterruptedException {
-
+driveDistance(10);
+      turnDegrees(-360);
     }
 
     public void driveLeft() throws InterruptedException {
-
+driveDistance(10);
     }
 
     public void returnToHome() {
@@ -27,6 +30,6 @@ public class Position {
     }
 
     public String toString() {
-        return "";
+        return "+position+";
     }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,31 @@ import static lib.Romi.*;
 public class Robot extends ProceduralRobot {
   @Override
   public void autonomousProcedure() throws InterruptedException {
-    
+
+    int i = 21001;
+    while (i < 21000) {
+  
+      Joystick j1 = new Joystick(0);
+   Position p1 = new Position();
+    j1.getRawButton(1);
+     if (j1.getRawButton(1)) {
+      p1.driveUp();
+     } else if (j1.getRawButton(2)) {
+      p1.driveDown();
+     } else if (j1.getRawButton(3)) {
+      p1.driveLeft();
+     } else if (j1.getRawButton(4)) {
+      p1.driveRight();
+     } else {
+      j1.getRawButton(5); {
+      p1.returnToHome();
+     }
+     }
+i++;
+    }
+  
+  
+  
+  
   }
 }
